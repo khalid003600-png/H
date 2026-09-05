808804e86986bc87bb1a4b7bee697ef9c7450339
khalid003600-png <khalid003600@gmail.com>
2026-09-05 05:16:07 +0300

    feat: add secure network pairing profiles

---
+++ WFNetworkPairingStore.h
@@ -0,0 +1,37 @@
+// WFNetworkPairingStore.h - Secure pairing profiles for WolFox 1.8.4
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef NS_ENUM(NSInteger, WFNetworkProfileKind) {
+    WFNetworkProfileKindRadar = 0,
+    WFNetworkProfileKindWiFi = 1,
+    WFNetworkProfileKindTLS = 2,
+};
+
+@interface WFNetworkPairingProfile : NSObject <NSCopying>
+@property (nonatomic, copy) NSString *profileID;
+@property (nonatomic, assign) WFNetworkProfileKind kind;
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *host;
+@property (nonatomic, assign) NSInteger port;
+@property (nonatomic, copy) NSString *deviceIdentifier;
+@property (nonatomic, copy) NSString *ssid;
+@property (nonatomic, copy) NSString *certificateSHA256;
+@property (nonatomic, assign) BOOL autoReconnect;
+@property (nonatomic, strong) NSDate *updatedAt;
+@end
+
+@interface WFNetworkPairingStore : NSObject
++ (instancetype)shared;
+@property (nonatomic, readonly, copy) NSArray<WFNetworkPairingProfile *> *profiles;
+- (BOOL)saveProfile:(WFNetworkPairingProfile *)profile
+             secret:(nullable NSString *)secret
+              error:(NSError * _Nullable * _Nullable)error;
+- (BOOL)deleteProfileID:(NSString *)profileID error:(NSError * _Nullable * _Nullable)error;
+- (nullable WFNetworkPairingProfile *)profileWithID:(NSString *)profileID;
+- (nullable NSString *)secretForProfileID:(NSString *)profileID
+                                    error:(NSError * _Nullable * _Nullable)error;
+@end
+
+NS_ASSUME_NONNULL_END
