53f8aa39ce97ff873759e9b5caa72054858de6ab
khalid003600-png <khalid003600@gmail.com>
2026-09-10 00:40:07 +0300

    Add shared K7GPS enhancement helpers

---
+++ K7GPS/K7GPSEnhancements.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+@interface K7GPSEnhancements : NSObject
++ (NSDictionary *)buildInformationForVariant:(NSString *)variant version:(NSString *)version;
++ (nullable NSDictionary *)coordinateFromText:(NSString *)text;
++ (NSData *)exportSettingsDictionary:(NSDictionary *)settings error:(NSError **)error;
++ (nullable NSDictionary *)importSettingsData:(NSData *)data error:(NSError **)error;
+@end
+NS_ASSUME_NONNULL_END
