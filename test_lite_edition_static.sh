6b0e3cdf6a9360cff690be650ad954c763b99f01
khalid003600-png <khalid003600@gmail.com>
2026-09-09 15:07:22 +0300

    fix(tests): validate current generated version define

--- test_lite_edition_static.sh
+++ test_lite_edition_static.sh
@@ -15,7 +15,7 @@ check "$MASTER" "if (candidate.tag == page)" "تنقل Lite الصحيح بين 
 check "$MASTER" "BOOL active = store.spoofActive;" "حالة Lite تعتمد على تزييف الموقع فقط"
 check "$BUILD" 'COMMON_FLAGS+=(-DWOLFOX_LITE=1)' "علامة Lite تضاف أثناء الترجمة"
 check "$BUILD" 'PACKAGE_ID="com.wolfox.gpspro.lite"' "معرّف تثبيت مستقل لنسخة Lite"
-check "$BUILD" 'printf '"'"'#define WF_TWEAK_VERSION @"%s"' "حقن رقم الإصدار حسب عملية البناء"
+check "$BUILD" '#define WF_TWEAK_VERSION @"$(escape_objc_string "$VERSION")"' "حقن رقم الإصدار حسب عملية البناء"
 check "$LITE_BUILD" 'WOLFOX_VERSION="1.8.6-Lite"' "رقم Lite المستقل 1.8.6"
 check "$LITE_BUILD" 'exec ./build_v1_deb.sh' "Lite تستخدم نفس مسار المصدر والبناء"
 
