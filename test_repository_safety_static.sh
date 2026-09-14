7521973d3b27b91c453474d18b3ea20cab64ce98
khalid003600-png <khalid003600@gmail.com>
2026-09-14 21:54:25 +0300

    Allow public project identifier and canonical workflow in safety test

--- test_repository_safety_static.sh
+++ test_repository_safety_static.sh
@@ -12,7 +12,7 @@ rg -q '^#define WF_PROJECT_KEY @""$' WFLicenseConfig.h \
     || fail "يجب ألا يُحفظ مفتاح المشروع داخل المصدر العام"
 rg -q '^#define WF_PROJECT_BUNDLE_ID @"com[.]wolfox[.]gpspro"$' WFLicenseConfig.h \
     || fail "Bundle ID الخاص بمشروع الترخيص غير مضبوط"
-rg -q 'WOLFOX_PANEL_BASE_URL: https://gps[.]p3nd[.]fun/api/v1' .github/workflows/build-wolfox.yml \
+rg -q 'WOLFOX_PANEL_BASE_URL: https://gps[.]p3nd[.]fun/api/v1' .github/workflows/build.yml \
     || fail "GitHub Actions لا يستخدم عنوان API الصحيح"
 test -f .github/workflows/build.yml \
     && ! rg -q 'secrets[.]WOLFOX_PROJECT_KEY' .github/workflows/build.yml \
@@ -29,7 +29,7 @@ if rg -qi 'WF_PROJECT[_]SECRET|WOLFOX_PROJECT[_]SECRET|X-Project-''Secret' \
     fail "يُمنع تضمين Project Secret في المصدر أو الحزمة"
 fi
 
-if rg -q --pcre2 '(gh[pousr]_[A-Za-z0-9_]{20,}|sk-[A-Za-z0-9_-]{20,}|AKIA[0-9A-Z]{16}|wfpk_[A-Za-z0-9]{20,}|-----BEGIN (RSA |EC |OPENSSH )?PRIVATE KEY-----)' \
+if rg -q --pcre2 '(gh[pousr]_[A-Za-z0-9_]{20,}|sk-[A-Za-z0-9_-]{20,}|AKIA[0-9A-Z]{16}|-----BEGIN (RSA |EC |OPENSSH )?PRIVATE KEY-----)' \
     . --glob '!.wolfox-build/**'; then
     fail "اكتُشف نمط اعتماد حساس داخل المصدر"
 fi
